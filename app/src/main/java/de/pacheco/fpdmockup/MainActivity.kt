7a74a04b688896ecb6ec395074ca903abc438324
pacheco <pachecoberlin@gmail.com>
2025-03-20 14:24:57 +0100

    Added and changed video

--- app/src/main/java/de/pacheco/fpdmockup/MainActivity.kt
+++ app/src/main/java/de/pacheco/fpdmockup/MainActivity.kt
@@ -23,30 +23,37 @@ import androidx.media3.ui.PlayerView
 class MainActivity : AppCompatActivity() {
     private var player: ExoPlayer? = null
     private var videoContainer: FrameLayout? = null
+    private var hideUi: View? = null
     private var dpi: EditText? = null
     private var heightInput: EditText? = null
-    private var aspectRatioHeightInput: EditText? = null
     private var aspectRatioWidthInput: EditText? = null
+    private var aspectRatioHeightInput: EditText? = null
     private var formatSwitch: Switch? = null
     private var playerView: PlayerView? = null
     private var btn12_21_9: View? = null
     private var btn12_16_9: View? = null
     private var btn15_175_9: View? = null
     private var btn12_175_9: View? = null
-    private var hideUi: View? = null
     private var isTypingText = false
-
-    private val mediaItem16_9: MediaItem =
-        MediaItem.fromUri(
-            Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-                .path(R.raw.bbb16.toString()).build()
-        )
-
-    private val mediaItem21_9 =
-        MediaItem.fromUri(
-            Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-                .path(R.raw.bbb21.toString()).build()
-        )
+    private var changeVideo: View? = null
+    private val sintel219: MediaItem = MediaItem.fromUri(
+        Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .path(R.raw.sintel219.toString()).build()
+    )
+    private val sintel169 = MediaItem.fromUri(
+        Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .path(R.raw.sintel169.toString()).build()
+    )
+    private val spring169: MediaItem = MediaItem.fromUri(
+        Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .path(R.raw.spring169.toString()).build()
+    )
+    private val spring219 = MediaItem.fromUri(
+        Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .path(R.raw.spring219.toString()).build()
+    )
+    private var mediaItem16_9: MediaItem = spring169
+    private var mediaItem21_9: MediaItem = spring219
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,24 +62,27 @@ class MainActivity : AppCompatActivity() {
 
         playerView = findViewById<PlayerView?>(R.id.player_view)
         videoContainer = findViewById<FrameLayout?>(R.id.video_container)
+        hideUi = findViewById<View?>(R.id.hideUI)
+        dpi = findViewById<EditText?>(R.id.dpi)
         heightInput = findViewById<EditText?>(R.id.height_input)
         aspectRatioWidthInput = findViewById<EditText?>(R.id.aspect_ratio_width_input)
         aspectRatioHeightInput = findViewById<EditText?>(R.id.aspect_ratio_height_input)
         formatSwitch = findViewById<Switch?>(R.id.format_switch)
-        dpi = findViewById<EditText?>(R.id.dpi)
         btn12_21_9 = findViewById<View?>(R.id.button_12cm_21_9)
         btn12_16_9 = findViewById<View?>(R.id.button_12cm_16_9)
         btn15_175_9 = findViewById<View?>(R.id.button_15cm_17_5_9)
         btn12_175_9 = findViewById<View?>(R.id.button_12cm_17_5_9)
-        hideUi = findViewById<View?>(R.id.hideUI)
+        btn12_175_9 = findViewById<View?>(R.id.button_12cm_17_5_9)
+        changeVideo = findViewById<View?>(R.id.changeVideo)
 
         player = ExoPlayer.Builder(this).build()
         playerView?.setPlayer(player)
 
-//    val videoUri: Uri =
+//      val videoUri: Uri =
 //        android.net.Uri.parse(
 //            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
-//    val mediaItem: MediaItem = MediaItem.fromUri(videoUri)
+//      val mediaItem: MediaItem = MediaItem.fromUri(videoUri)
+
         player?.setMediaItem(mediaItem16_9)
         player?.prepare()
         player?.play()
@@ -80,7 +90,10 @@ class MainActivity : AppCompatActivity() {
         setupButtons()
         setupTextWatchers()
         setupSwitch()
+
+        isTypingText = true
         updateBoxSize(12.0, 21.0, 9.0)
+        isTypingText = false
     }
 
     private fun hideSystemUI() {
@@ -91,6 +104,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupButtons() {
+        changeVideo?.setOnClickListener{_ ->
+            changeVideo()
+        }
+        hideUi?.setOnClickListener { v: View? ->
+            val ui = findViewById<View?>(R.id.ui)
+            ui.visibility = if (ui.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
         btn12_21_9?.setOnClickListener { v: View? ->
             updateBoxSize(12.0, 21.0, 9.0)
         }
@@ -103,32 +123,35 @@ class MainActivity : AppCompatActivity() {
         btn12_175_9?.setOnClickListener { v: View? ->
             updateBoxSize(12.0, 17.5, 9.0)
         }
-        hideUi?.setOnClickListener { v: View? ->
-            val ui = findViewById<View?>(R.id.ui)
-            ui.visibility = if (ui.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+    }
+
+    private fun changeVideo() {
+        if (mediaItem16_9 == spring169) {
+            mediaItem16_9 = sintel169
+            mediaItem21_9 = sintel219
+        } else {
+            mediaItem16_9 = spring169
+            mediaItem21_9 = spring219
         }
+        updatePlayer()
     }
 
     private fun setupTextWatchers() {
-        val textWatcher: TextWatcher =
-            object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                }
-
-                override fun afterTextChanged(s: Editable?) {
-                    isTypingText = true
-                    updateBoxFromInputs()
-                    isTypingText = false
-                }
+        val textWatcher: TextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?, start: Int, count: Int, after: Int
+            ) {
             }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                isTypingText = true
+                updateBoxFromInputs()
+                isTypingText = false
+            }
+        }
 
         dpi?.addTextChangedListener(textWatcher)
         heightInput?.addTextChangedListener(textWatcher)
@@ -142,30 +165,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupSwitch() {
-        formatSwitch?.setOnCheckedChangeListener(
-            CompoundButton.OnCheckedChangeListener { buttonView: CompoundButton?,
-                                                     isChecked: Boolean ->
-                formatSwitch?.setText(if (isChecked) "21:9" else "16:9")
-                updateVideoFormat()
-            })
+        formatSwitch?.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView: CompoundButton?, isChecked: Boolean ->
+            formatSwitch?.setText(if (isChecked) "21:9" else "16:9")
+            updatePlayer()
+        })
     }
 
     private fun updateBoxFromInputs() {
         try {
-            var height = heightInput?.getText().toString()
-            val heightCm: Double = height.toDouble()
+            val heightCm: Double = heightInput?.getText().toString().toDouble()
             val aspectRatioWidth: Double = aspectRatioWidthInput?.getText().toString().toDouble()
             val aspectRatioHeight: Double = aspectRatioHeightInput?.getText().toString().toDouble()
             updateBoxSize(heightCm, aspectRatioWidth, aspectRatioHeight)
         } catch (e: NumberFormatException) {
-            Toast.makeText(this, "Wrong Format", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "Wrong Format", Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun updateBoxSize(
-        heightCm: Double,
-        aspectRatioWidth: Double,
-        aspectRatioHeight: Double
+        heightCm: Double, aspectRatioWidth: Double, aspectRatioHeight: Double
     ) {
 //      val densityDpi = getResources().getDisplayMetrics().densityDpi //das ist falsch wenn die im AOSP falsch eingetragen sind
         val metrics = DisplayMetrics()
@@ -186,9 +204,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateTextFields(
-        heightCm: Double,
-        aspectRatioWidth: Double,
-        aspectRatioHeight: Double
+        heightCm: Double, aspectRatioWidth: Double, aspectRatioHeight: Double
     ) {
         if (isTypingText) return
         heightInput?.setText(heightCm.toString())
@@ -196,7 +212,7 @@ class MainActivity : AppCompatActivity() {
         aspectRatioHeightInput?.setText(aspectRatioHeight.toString())
     }
 
-    private fun updateVideoFormat() {
+    private fun updatePlayer() {
         val currentPosition = player?.currentPosition ?: 0L
         val wasPlaying = player?.isPlaying ?: true
         if ("21:9" == formatSwitch?.getText()) {
